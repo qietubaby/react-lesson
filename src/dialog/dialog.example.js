26b7128b0aab1d3faead6e78e3da96e85bb397de
qietubaby <201951244@qq.com>
2021-10-31 12:30:49 +0800

    feat: 添加dialog api

--- src/dialog/dialog.example.js
+++ src/dialog/dialog.example.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Dialog, {alert} from "./dialog";
+import Dialog, {alert, confirm} from "./dialog";
 
 const DialogExample = () => {
   const [x, setX] = useState(false);
@@ -33,9 +33,15 @@ const DialogExample = () => {
         }}>直接调用的组件alert</button>
   
       </div>
+
+
       <div style={{ border: "1px solid red" }}>
         <button onClick={() => {
-          return alert('1')
+          return confirm('1', () => {
+            console.log('你点击了yes');
+          }, () => {
+            console.log('你点击了no')
+          })
         }}>confirm</button>
   
       </div>
--- src/dialog/dialog.js
+++ src/dialog/dialog.js
@@ -70,6 +70,42 @@ const alert = (content) => {
   ReactDOM.render(component, div);
 };
 
-export {alert};
+
+/*
+  创建 confirm api，使用Dialog组件进行二次改造
+*/
+
+const confirm = (content, yes, no) => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove();
+    yes && yes();
+  }
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    no && no();
+  }
+  const component = (
+    <Dialog 
+      visible={true}
+      onClose={onNo}
+      buttons={[
+        <button onClick={onYes}>yes</button>,
+        <button onClick={onNo}>no</button>
+      ]}
+      >{content}
+    </Dialog>)
+  const div = document.createElement('div');
+  document.body.append(div);
+  ReactDOM.render(component, div);
+}
+
+export {alert, confirm};
 
 export default Dialog;
+
+
+
